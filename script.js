2b5763015afc89d3a1963e800232c9be1d3862ef
Nathan <urwin.nathan@gmail.com>
2024-06-25 11:29:33 +0100

    Complete Basic Calculator

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
 // add 2 nums
 const add = function (num1, num2) {
-	console.log(num1 + num2);
+	//console.log(num1 + num2);
 	return num1 + num2;
 };
 
 // subtract 2 nums
 const subtract = function (num1, num2) {
-	console.log(num1 - num2);
+	//console.log(num1 - num2);
 	return num1 - num2;
 };
 
 // multiply 2 nums
 const multiply = function (num1, num2) {
-	console.log(num1 * num2);
+	//console.log(num1 * num2);
 	return num1 * num2;
 };
 
 // divide 2 nums
 const divide = function (num1, num2) {
 	// Catch divide by 0
-	if (!num2) {
+	if (num2 === "0") {
 		return "Nice try nerd";
-	}
-	console.log(num1 / num2);
-	return num1 / num2;
+	} else {
+        //console.log(num1 / num2);
+	    return num1 / num2;
+    }
 };
 
 // Operate function used for equals button
 
 const operate = function (num1, num2, operator) {
-	console.log(operator(num1, num2));
+	//console.log(operator(num1, num2));
 	return operator(num1, num2);
 };
 
@@ -40,14 +41,14 @@ let displayVal = "";
 const calcDisplay = document.querySelector("#display");
 
 const updateDisplay = function (val) {
-	console.log(`Val is = ${val}`);
-	console.log(`Val type is = ${typeof val}`);
+	//console.log(`Val is = ${val}`);
+	//console.log(`Val type is = ${typeof val}`);
 	displayVal = displayVal.concat(val);
 	// round to 35 chars
 	if (displayVal.length > 35) {
 		displayVal = displayVal.slice(0, 36);
 	}
-	console.log(`displayVal is = ${displayVal}`);
+	//console.log(`displayVal is = ${displayVal}`);
 	calcDisplay.textContent = displayVal;
 };
 
@@ -67,25 +68,27 @@ const subtractButton = document.querySelector("#subtract");
 const multiplyButton = document.querySelector("#multiply");
 const divideButton = document.querySelector("#divide");
 addButton.addEventListener("click", () => {
-    updateDisplay("+");
-})
+	updateDisplay("+");
+});
 subtractButton.addEventListener("click", () => {
-    updateDisplay("-");
-})
+	updateDisplay("-");
+});
 multiplyButton.addEventListener("click", () => {
-    updateDisplay("*");
-})
+	updateDisplay("*");
+});
 divideButton.addEventListener("click", () => {
-    updateDisplay("/");
-})
+	updateDisplay("/");
+});
 
 // When equals button is clicked, calculate operation
 const equalsButton = document.querySelector("#equals");
 
-// vars for storing display numbers ready for calculation
-let firstNum = 0;
-let secondNum = 0;
+// vars for storing display numbers and ops ready for calculation
+let firstNum = undefined;
+let secondNum = undefined;
 let operator = undefined;
+let operatorSign = undefined;
+let result = undefined;
 // Regex to read nums
 const regexNums = /\d+/;
 // Regex to read operators
@@ -93,20 +96,62 @@ const regexOps = /[+\-*/]/;
 
 equalsButton.addEventListener("click", () => {
 	// Use operator regex to split display into nums/ops
-        // read num1
-        firstNum = 3;
-        // read num2
-        secondNum = 56;
-        // read operator
-        operator = multiply;
+	let displayArray = displayVal.split(regexOps);
+	//console.log(displayArray);
+	// read num1
+	firstNum = displayArray[0];
+	// read num2
+	secondNum = displayArray[1];
+	// read operatorSign
+	operatorSign = displayVal.match(regexOps);
+	operatorSign = operatorSign.toString().trim();
+	//console.log(`Operator is ${operatorSign} of type: ${typeof operatorSign}`);
+	// convert operator sign into function
+	switch (operatorSign) {
+		case "+":
+			//console.log("Add");
+			// clear display
+			clearDisplay();
+			// update display with new result
+            result = operate(firstNum, secondNum, add);
+			updateDisplay(result);
+			break;
+		case "-":
+			//console.log("Subtract");
+            // clear display
+			clearDisplay();
+			// update display with new result
+            result = operate(firstNum, secondNum, subtract);
+			updateDisplay(result);
+			break;
+		case "*":
+			//console.log("Multiply");
+            // clear display
+			clearDisplay();
+			// update display with new result
+            result = operate(firstNum, secondNum, multiply);
+			updateDisplay(result);
+			break;
+		case "/":
+			//console.log("Divide");
+            // clear display
+			clearDisplay();
+			// update display with new result
+            result = operate(firstNum, secondNum, divide);
+			updateDisplay(result);
+			break;
+	}
 	/* 
     Need to add exceptions for if display is invalid 
     - lacking two numbers separated by an operator
     */
-    // clear display
-    clearDisplay();
-    // update display with new result
-	updateDisplay(operate(firstNum, secondNum, operator));
+
+	// clear saved values
+	firstNum = undefined;
+	secondNum = undefined;
+	operatorSign = undefined;
+    operator = undefined;
+    result = undefined;
 });
 
 // When clear button is clicked, delete displayVals and update
@@ -114,9 +159,9 @@ const clearButton = document.querySelector("#clear");
 
 // reusable clear function
 const clearDisplay = function () {
-    displayVal = "";
+	displayVal = "";
 	calcDisplay.textContent = displayVal;
-}
+};
 
 clearButton.addEventListener("click", () => {
 	clearDisplay();
